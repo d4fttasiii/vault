29fa494c1c5c4df97edc51983e86a7beb0bde9bc
Peter Tasner <peter.tasner@conceptualise.de>
2022-08-01 17:05:47 +0200

    Extending contract functionality

--- src/contracts/vault/programs/vault/src/lib.rs
+++ src/contracts/vault/programs/vault/src/lib.rs
@@ -1,15 +1,215 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("GWDtSYERr74SqwyirLDfUBS15Hnp6gwsp49qtTQkwJhs");
+
+pub const PROFILE_PDA_SEED: &[u8] = b"vault-profile";
+pub const PROFILE_DOCUMENT_PDA_SEED: &[u8] = b"vault-document";
+pub const PROFILE_DOCUMENT_SHARE_PDA_SEED: &[u8] = b"vault-document-share";
 
 #[program]
 pub mod vault {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_profile(ctx: Context<CreateProfile>) -> Result<()> {
+        let profile_account = &mut ctx.accounts.profile_data;
+        profile_account.profile = ctx.accounts.profile.key();
+        profile_account.created = Clock::get()?.unix_timestamp;
+        profile_account.document_count = 0;
+        
         Ok(())
     }
+
+    pub fn create_profile_document(ctx: Context<CreateProfileDocument>, name: String) -> Result<()> {
+        let profile = &mut ctx.accounts.profile_data;
+        if profile.profile.key() != ctx.accounts.profile.key() {
+            return err!(ErrorCode::OnlyProfileOwnerCanAccess);
+        }
+
+        let document = &mut ctx.accounts.document;
+        document.created = Clock::get()?.unix_timestamp;
+        document.profile = ctx.accounts.profile.key();
+        document.name = name;
+        document.index = profile.document_count;
+
+        profile.document_count += 1;
+        profile.updated = Clock::get()?.unix_timestamp;
+       
+        Ok(())
+    }
+
+    pub fn create_profile_document_share(
+        ctx: Context<CreateProfileDocumentShare>,
+        _document_index: u64, 
+        invitee: Pubkey,
+        valid_until: i64) -> Result<()> {        
+
+        if ctx.accounts.profile.key() != ctx.accounts.document.profile.key() {
+            return err!(ErrorCode::OnlyProfileOwnerCanAccess);
+        }
+
+        let document_share = &mut ctx.accounts.document_share;
+        document_share.created = Clock::get()?.unix_timestamp;
+        document_share.updated = Clock::get()?.unix_timestamp;
+        document_share.document_index = ctx.accounts.document.index;
+        document_share.is_public = false;
+        document_share.is_active = true;
+        document_share.invitee = invitee.key();
+        document_share.valid_until = valid_until;
+
+        Ok(())
+    }
+
+    pub fn toggle_profile_document_share(
+        ctx: Context<ToggleProfileDocumentShare>, 
+        _document_index: u64, 
+        _invitee: Pubkey,) -> Result<()> {        
+
+        if ctx.accounts.profile.key() != ctx.accounts.document.profile.key() {
+            return err!(ErrorCode::OnlyProfileOwnerCanAccess);
+        }
+
+        let document_share = &mut ctx.accounts.document_share;
+        document_share.updated = Clock::get()?.unix_timestamp;
+        document_share.is_active = !document_share.is_active;
+
+        Ok(())
+    }
+
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateProfile<'info> {
+    #[account(mut)]
+    pub profile: Signer<'info>,
+    #[account(
+        init, 
+        payer = profile,
+        space = PROFILE_DATA_LEN,
+        seeds = [
+            PROFILE_PDA_SEED.as_ref(), 
+            profile.key().as_ref()
+        ], 
+        bump,
+    )]
+    pub profile_data: Account<'info, ProfileData>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CreateProfileDocument<'info> {
+    #[account(mut)]
+    pub profile: Signer<'info>,
+    #[account(
+        init, 
+        payer = profile,
+        space = DOCUMENT_LEN,
+        seeds = [
+            PROFILE_DOCUMENT_PDA_SEED.as_ref(), 
+            profile.key().as_ref(), 
+            profile_data.document_count.to_string().as_bytes().as_ref()
+        ], 
+        bump,
+    )]
+    pub document: Account<'info, DocumentData>,
+    #[account(
+        mut,
+        seeds = [PROFILE_PDA_SEED.as_ref(), profile.key().as_ref()], 
+        bump
+    )]
+    pub profile_data: Account<'info, ProfileData>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(document_index: u64, invitee: Pubkey)]
+pub struct CreateProfileDocumentShare<'info> {
+    #[account(mut)]
+    pub profile: Signer<'info>,
+    #[account(
+        init, 
+        payer = profile,
+        space = DOCUMENT_SHARE_LEN,
+        seeds = [
+            document.key().as_ref(),
+            PROFILE_DOCUMENT_SHARE_PDA_SEED,
+            invitee.key().as_ref()
+        ], 
+        bump,
+    )]
+    pub document_share: Account<'info, DocumentShareData>,
+    #[account(
+        mut,
+        seeds = [
+            PROFILE_DOCUMENT_PDA_SEED.as_ref(), 
+            profile.key().as_ref(), 
+            document_index.to_string().as_bytes().as_ref()
+        ], 
+        bump
+    )]
+    pub document: Account<'info, DocumentData>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(document_index: u64, invitee: Pubkey)]
+pub struct ToggleProfileDocumentShare<'info> {
+    #[account(mut)]
+    pub profile: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [
+            document.key().as_ref(),
+            PROFILE_DOCUMENT_SHARE_PDA_SEED,
+            invitee.key().as_ref()
+        ], 
+        bump,
+    )]
+    pub document_share: Account<'info, DocumentShareData>,
+    #[account(
+        mut,
+        seeds = [
+            PROFILE_DOCUMENT_PDA_SEED.as_ref(), 
+            profile.key().as_ref(), 
+            document_index.to_string().as_bytes().as_ref()
+        ], 
+        bump
+    )]
+    pub document: Account<'info, DocumentData>,
+    pub system_program: Program<'info, System>,
+}
+
+pub const PROFILE_DATA_LEN: usize = 32 + 8 + 8 + 8 + 8;
+#[account]
+pub struct ProfileData {
+    pub profile: Pubkey,
+    pub document_count: u64,
+    pub created: i64,
+    pub updated: i64,
+}
+
+pub const DOCUMENT_LEN: usize = 32 + (4 + 64) + 8 + 8 + 8;
+#[account]
+pub struct DocumentData {
+    pub profile: Pubkey,
+    pub name: String,
+    pub index: u64,
+    pub created: i64,
+}
+
+pub const DOCUMENT_SHARE_LEN: usize = 32 + 8 + 8 + 8 + 8 + 1 + 1 + 8;
+#[account]
+pub struct DocumentShareData {
+    pub invitee: Pubkey,
+    pub document_index: u64,
+    pub created: i64,
+    pub updated: i64,
+    pub valid_until: i64,
+    pub is_public: bool,
+    pub is_active: bool,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("OnlyProfileOwnerCanAccess")]
+    OnlyProfileOwnerCanAccess,
+}
--- src/contracts/vault/tests/vault.ts
+++ src/contracts/vault/tests/vault.ts
@@ -1,16 +1,142 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { Program, BN } from "@project-serum/anchor";
+import { PublicKey, Keypair } from '@solana/web3.js';
 import { Vault } from "../target/types/vault";
+import { expect } from "chai";
 
 describe("vault", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
+  const bob = new Keypair();
   const program = anchor.workspace.Vault as Program<Vault>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const getProfilePda = async (address: PublicKey): Promise<PublicKey> => {
+    const [pda, _] = await PublicKey
+      .findProgramAddress(
+        [
+          anchor.utils.bytes.utf8.encode('vault-profile'),
+          address.toBuffer(),
+        ],
+        program.programId
+      );
+
+    return pda;
+  };
+
+  const getProfileDocumentPda = async (address: PublicKey, docCount: number): Promise<PublicKey> => {
+    const [pda, _] = await PublicKey
+      .findProgramAddress(
+        [
+          anchor.utils.bytes.utf8.encode('vault-document'),
+          address.toBuffer(),
+          anchor.utils.bytes.utf8.encode(docCount.toString()),
+        ],
+        program.programId
+      );
+
+    return pda;
+  };
+
+  const getProfileDocumentSharePda = async (documentPda: PublicKey, invitee: PublicKey): Promise<PublicKey> => {
+    const [pda, _] = await PublicKey
+      .findProgramAddress(
+        [
+          documentPda.toBuffer(),
+          anchor.utils.bytes.utf8.encode('vault-document-share'),
+          invitee.toBuffer(),
+        ],
+        program.programId
+      );
+
+    return pda;
+  };
+
+  it("Creates Profile", async () => {
+    const profilPda = await getProfilePda(provider.wallet.publicKey);
+
+    await program.methods
+      .createProfile()
+      .accounts({
+        profile: provider.wallet.publicKey,
+        profileData: profilPda,
+      })
+      .rpc();
+
+    expect((await program.account.profileData.fetch(profilPda)).profile)
+      .to
+      .be
+      .eql(provider.wallet.publicKey);
+
+    expect((await program.account.profileData.fetch(profilPda)).documentCount.toNumber())
+      .to
+      .be
+      .eql(0);
   });
+
+  it("Upload 5 Documents", async () => {
+    const profilPda = await getProfilePda(provider.wallet.publicKey);
+
+    for (let i = 0; i < 5; i++) {
+      const profileDocumentPda = await getProfileDocumentPda(provider.wallet.publicKey, i);
+      await program.methods
+        .createProfileDocument(`asd-${i}.jpg`)
+        .accounts({
+          profile: provider.wallet.publicKey,
+          profileData: profilPda,
+          document: profileDocumentPda
+        })
+        .rpc();
+    }
+
+    expect((await program.account.profileData.fetch(profilPda)).documentCount.toNumber())
+      .to
+      .be
+      .eql(5);
+  });
+
+  it("Sharing the 2nd document", async () => {
+    const profileDocumentPda = await getProfileDocumentPda(provider.wallet.publicKey, 2);
+    const profileDocumentSharePda = await getProfileDocumentSharePda(profileDocumentPda, bob.publicKey);
+
+    await program.methods
+      .createProfileDocumentShare(new BN(2), bob.publicKey, new BN(8))
+      .accounts({
+        profile: provider.wallet.publicKey,
+        document: profileDocumentPda,
+        documentShare: profileDocumentSharePda,
+      })
+      .rpc();
+
+    expect((await program.account.documentShareData.fetch(profileDocumentSharePda)).documentIndex.toNumber())
+      .to
+      .be
+      .eql(2);
+
+    expect((await program.account.documentShareData.fetch(profileDocumentSharePda)).invitee.toBase58())
+      .to
+      .be
+      .eql(bob.publicKey.toBase58());
+  });
+
+
+  it("Revoke 2nd document share", async () => {
+    const profileDocumentPda = await getProfileDocumentPda(provider.wallet.publicKey, 2);
+    const profileDocumentSharePda = await getProfileDocumentSharePda(profileDocumentPda, bob.publicKey);
+
+    await program.methods
+      .toggleProfileDocumentShare(new BN(2), bob.publicKey)
+      .accounts({
+        profile: provider.wallet.publicKey,
+        document: profileDocumentPda,
+        documentShare: profileDocumentSharePda,
+      })
+      .rpc();
+
+    expect((await program.account.documentShareData.fetch(profileDocumentSharePda)).isActive)
+      .to
+      .be
+      .eql(false);
+  });
+
 });
